cc2054db71bdb2b2948c311b8788d74f78437386
Edvaldo Vitor <edvaldovitor250@.gmail.com>
2024-01-29 19:35:25 -0300

    project finished

--- projetc/assets/main.js
+++ projetc/assets/main.js
@@ -1,85 +1,97 @@
 function exibirResultado(cepData) {
     var resultadoDiv = document.getElementById('resultadoCep');
-    resultadoDiv.innerHTML = ''; 
+    resultadoDiv.innerHTML = '';
 
     if (cepData.erro) {
         resultadoDiv.innerHTML = '<p class="text-center">Cep não encontrado.</p>';
     } else {
         const bandeiras = {
-            'AC': './assets/img/acre.png',
-            'AL': './assets/img/alagoas.png',
-            'AM': './assets/img/amazonas.png',
-            'AP': './assets/img/amapa.png',
-            'BA': './assets/img/bahia.png',
-            'CE': './assets/img/ceara.png',
-            'DF': './assets/img/distrito-federal.png',
-            'ES': './assets/img/espirito-santo.png',
-            'GO': './assets/img/goias.png',
-            'MA': './assets/img/maranhao.png',
-            'MG': './assets/img/minas-gerais.png',
-            'MS': './assets/img/mato-gross.png',
-            'MT': './assets/img/mato-grosso.png', 
-            'PA': './assets/img/para.png',
-            'PB': './assets/img/paraiba.png',
-            'PE': './assets/img/pernambuco.png',
-            'PI': './assets/img/piaui.png',
-            'PR': './assets/img/parana.png',
-            'RJ': './assets/img/rio-de-janeiro.png',
-            'RN': './assets/img/rio-grande-norte.png',
-            'RO': './assets/img/rondonia.png',
-            'RR': './assets/img/roraima.png',
-            'RS': './assets/img/rio-grande-do-sul.png',
-            'SC': './assets/img/santa-catarina.png',
-            'SE': './assets/img/sergipe.png',
-            'SP': './assets/img/sao-paulo.png',
-            'TO': './assets/img/tocantins.png',
-        };
+            'AC': './projetc/assets/img/acre.png', 
+            'AL': './projetc/assets/img/alagoas.png',
+            'AM': './projetc/assets/img/amazonas.png',
+            'AP': './projetc/assets/img/amapa.png',
+            'BA': './projetc/assets/img/bahia.png',
+            'CE': './projetc/assets/img/ceara.png',
+            'DF': './projetc/assets/img/distrito-federal.png',
+            'ES': './projetc/assets/img/espirito-santo.png',
+            'GO': './projetc/assets/img/goias.png',
+            'MA': './projetc/assets/img/maranhao.png',
+            'MG': './projetc/assets/img/minas-gerais.png',
+            'MS': './projetc/assets/img/mato-gross.png',
+            'MT': './projetc/assets/img/mato-grosso.png',
+            'PA': './projetc/assets/img/para.png',
+            'PB': './projetc/assets/img/paraiba.png',
+            'PE': './projetc/assets/img/pernambuco.png',
+            'PI': './projetc/assets/img/piaui.png',
+            'PR': './projetc/assets/img/parana.png',
+            'RJ': './projetc/assets/img/rio-de-janeiro.png',
+            'RN': './projetc/assets/img/rio-grande-norte.png',
+            'RO': './projetc/assets/img/rondonia.png',
+            'RR': './projetc/assets/img/roraima.png',
+            'RS': './projetc/assets/img/rio-grande-do-sul.png',
+            'SC': './projetc/assets/img/santa-catarina.png',
+            'SE': './projetc/assets/img/sergipe.png',
+            'SP': './projetc/assets/img/sao-paulo.png',
+            'TO': './projetc/assets/img/tocantins.png',
+          };
 
         const estado = cepData.uf;
-        const bandeiraUrl = bandeiras[estado] || 'url_padrao.png'; 
+        const bandeiraUrl = bandeiras[estado] || 'url_padrao.png';
 
         resultadoDiv.innerHTML = `
-    <table class="table table-bordered mt-4 text-center styled-table">
-        <tr>
-            <th>CEP</th>
-            <td>${cepData.cep}</td>
-        </tr>
-        <tr>
-            <th>Logradouro</th>
-            <td>${cepData.logradouro}</td>
-        </tr>
-        <tr>
-            <th>Bairro</th>
-            <td>${cepData.bairro}</td>
-        </tr>
-        <tr>
-            <th>Cidade</th>
-            <td>${cepData.localidade}</td>
-        </tr>
-        <tr>
-            <th>Estado</th>
-            <td>${cepData.uf} <img src="${bandeiraUrl}" alt="Bandeira do Estado" style="max-width: 30px;"></td>
-        </tr>
-        <tr>
-            <th>DDD</th>
-            <td>${cepData.ddd}</td>
-        </tr>
-        <tr>
-            <th>Ibge</th>
-            <td>${cepData.ibge}</td>
-        </tr>
-    </table>
+        <div class="result-container-custom mt-4 mx-auto max-w-lg text-center transform transition duration-500 hover:scale-105"> 
+        <div class="overflow-x-auto">
+            <table class="w-full table-auto mx-auto border border-collapse border-blue-500">
+                <thead>
+                    <tr>
+                        <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">CEP</th>
+                        <td class="py-2 px-4 border-b text-center">${cepData.cep}</td>
+                    </tr>
+                    <tr>
+                        <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">Logradouro</th>
+                        <td class="py-2 px-4 border-b text-center">${cepData.logradouro}</td>
+                    </tr>
+                    <tr>
+                        <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">Bairro</th>
+                        <td class="py-2 px-4 border-b text-center">${cepData.bairro}</td>
+                    </tr>
+                    <tr>
+                        <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">Cidade</th>
+                        <td class="py-2 px-4 border-b text-center">${cepData.localidade}</td>
+                    </tr>
+                    <tr class="state-flag-info">
+                    <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">Estado</th>
+                    <td class="py-2 px-4 border-b text-right state-flag-cell">
+                        <div class="state-info">
+                            <span class="inline-block align-middle">${cepData.uf}</span> 
+                            <div class="state-flag">
+                                <img src="${bandeiraUrl}" alt="Bandeira do Estado" style="max-width: 30px;">
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                    <tr>
+                        <th class="py-2 px-4 border-b text-center bg-blue-500 text-white">DDD</th>
+                        <td class="py-2 px-4 border-b text-center">${cepData.ddd}</td>
+                    </tr>
+                    <tr>
+                        <th class="py-2 px-4 text-center bg-blue-500 text-white">Ibge</th>
+                        <td class="py-2 px-4 text-center">${cepData.ibge}</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 `;
 
-document.querySelectorAll('.styled-table th, .styled-table td').forEach(element => {
-    element.style.backgroundColor = '#007BFF'; 
-    element.style.color = '#fff'; 
-});
-
-document.getElementById('botoesAcoes').style.display = 'flex';
-
-    }
-}
+        document.querySelectorAll('.w-full th, .w-full td').forEach(element => {
+            element.style.backgroundColor = '#007BFF';
+            element.style.color = '#fff';
+            element.classList.add('transition', 'duration-500', 'hover:bg-blue-700');
+        });
+        
+        document.getElementById('botoesAcoes').style.display = 'flex';
+    }}
 
 document.getElementById('cepForm').addEventListener('submit', function (event) {
     event.preventDefault();
@@ -161,9 +173,6 @@ function reiniciarDados() {
 
     const cepInfoElement = document.getElementById('cepInfo');
     cepInfoElement.innerHTML = '';
-
-    const botoesAcoesContainer = document.getElementById('botoesAcoesContainer');
-    botoesAcoesContainer.style.display = 'none';
 
     const cepInput = document.getElementById('cepInput');
     cepInput.value = '';
